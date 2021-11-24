f2fde1b958b5d38947ca6245f99cdaff3b7968fa
Sainnhe Park <sainnhe@gmail.com>
2021-11-24 18:32:12 +0800

    Update website in comments

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 // Name:           CAJ2PDF Qt
 // Description:    CAJ 转 PDF 转换器
 // Author:         Sainnhe Park <sainnhe@gmail.com>
-// Website:        https://github.com/sainnhe/caj2pdf-qt
+// Website:        https://caj2pdf-qt.sainnhe.dev
 // License:        GPL3
 // -----------------------------------------------------------------------------
 
