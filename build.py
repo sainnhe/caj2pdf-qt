ea6cd3eabc3955d54ea8675f9fc231d886e08daa
Sainnhe Park <sainnhe@gmail.com>
2021-11-26 20:46:37 +0800

    Use mutool path to locate dll path

--- build.py
+++ build.py
@@ -34,25 +34,11 @@ os.chdir(workdir_caj2pdf)
 subprocess.run(["git", "apply", "../caj2pdf.diff"])
 subprocess.run(["python", "-m", "venv", "venv"])
 if platform.system() == "Windows":
-    copyfile(join(join(join(workdir_caj2pdf, "lib"), "bin"), "libjbigdec-w64.dll"),
-             join(workdir_caj2pdf, "libjbigdec"))
-    copyfile(join(join(join(workdir_caj2pdf, "lib"), "bin"), "libjbig2codec-w64.dll"),
-             join(workdir_caj2pdf, "libjbig2codec"))
     subprocess.run([".\\venv\\Scripts\\python.exe", "-m", "pip", "install", "--index-url=https://mirrors.aliyun.com/pypi/simple", "pypdf2", "pyinstaller"])
-    subprocess.run([".\\venv\\Scripts\\pyinstaller.exe", "-F",
-                    "--add-data", "libjbigdec;.",
-                    "--add-data", "libjbig2codec;.",
-                    "caj2pdf"])
+    subprocess.run([".\\venv\\Scripts\\pyinstaller.exe", "-F", "caj2pdf"])
 else:
-    copyfile(join(join(workdir_caj2pdf, "lib"), "libjbigdec.so"),
-             join(workdir_caj2pdf, "libjbigdec"))
-    copyfile(join(join(workdir_caj2pdf, "lib"), "libjbig2codec.so"),
-             join(workdir_caj2pdf, "libjbig2codec"))
     subprocess.run(["./venv/bin/python", "-m", "pip", "install", "--index-url=https://mirrors.aliyun.com/pypi/simple", "pypdf2", "pyinstaller"])
-    subprocess.run(["./venv/bin/pyinstaller", "-F",
-                    "--add-data", "libjbigdec:.",
-                    "--add-data", "libjbig2codec:.",
-                    "caj2pdf"])
+    subprocess.run(["./venv/bin/pyinstaller", "-F", "caj2pdf"])
 subprocess.run(["git", "checkout", "--", "."])
 
 # build mupdf
@@ -67,11 +53,6 @@ build_external_dir = join(build_dir, "external")
 src_dir = join(workdir, "src")
 os.mkdir(build_dir)
 os.mkdir(build_external_dir)
-if os.name != "nt":
-    move(join(join(workdir_caj2pdf, "dist"), "caj2pdf"),
-         join(build_external_dir, "caj2pdf"))
-    move(join(join(join(workdir_mupdf, "build"), "release"), "mutool"),
-         join(build_external_dir, "mutool"))
 os.chdir(src_dir)
 if platform.system() == "Windows":
     subprocess.run(["windres", "app.rc", "-o", "app.o"])
@@ -80,6 +61,10 @@ else:
     subprocess.run(["cmake", "--build", ".", "--config", "Release", "--", "--jobs=" + str(os.cpu_count())])
 os.chdir(build_dir)
 if platform.system() == "Darwin":
+    move(join(join(workdir_caj2pdf, "dist"), "caj2pdf"),
+         join(build_external_dir, "caj2pdf"))
+    move(join(join(join(workdir_mupdf, "build"), "release"), "mutool"),
+         join(build_external_dir, "mutool"))
     move(join(src_dir, "caj2pdf.app"),
          join(build_dir, "caj2pdf.app"))
     move(join(build_dir, "external"),
@@ -87,7 +72,26 @@ if platform.system() == "Darwin":
     os.mkdir(join(join(join(build_dir, "caj2pdf.app"), "Contents"), "Resources"))
     copyfile(join(join(workdir, "icons"), "convert.icns"),
              join(join(join(join(build_dir, "caj2pdf.app"), "Contents"), "Resources"), "convert.icns"))
+    copyfile(join(join(workdir_caj2pdf, "lib"), "libjbigdec.so"),
+         join(join(join(join(join(build_dir, "caj2pdf.app"), "Contents"), "MacOS"), "external"), "libjbigdec"))
+    copyfile(join(join(workdir_caj2pdf, "lib"), "libjbig2codec.so"),
+         join(join(join(join(join(build_dir, "caj2pdf.app"), "Contents"), "MacOS"), "external"), "libjbig2codec"))
     subprocess.run(["open", "."])
-elif platform.system() != "Windows":
+elif platform.system() == "Windows":
+    move(join(join(workdir_caj2pdf, "dist"), "caj2pdf.exe"),
+         join(build_external_dir, "caj2pdf.exe"))
+    copyfile(join(join(join(workdir_caj2pdf, "lib"), "bin"), "libjbigdec-w64.dll"),
+             join(join(build_dir, "external"), "libjbigdec"))
+    copyfile(join(join(join(workdir_caj2pdf, "lib"), "bin"), "libjbig2codec-w64.dll"),
+             join(join(build_dir, "external"), "libjbig2codec"))
+else:
+    move(join(join(workdir_caj2pdf, "dist"), "caj2pdf"),
+         join(build_external_dir, "caj2pdf"))
+    move(join(join(join(workdir_mupdf, "build"), "release"), "mutool"),
+         join(build_external_dir, "mutool"))
     move(join(src_dir, "caj2pdf"),
          join(build_dir, "caj2pdf"))
+    copyfile(join(join(workdir_caj2pdf, "lib"), "libjbigdec.so"),
+             join(join(build_dir, "external"), "libjbigdec"))
+    copyfile(join(join(workdir_caj2pdf, "lib"), "libjbig2codec.so"),
+             join(join(build_dir, "external"), "libjbig2codec"))
