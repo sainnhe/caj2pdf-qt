140a79aad05742104e820314d2003985ef4fb962
Sainnhe Park <i@sainnhe.dev>
2023-01-06 13:30:40 +0800

    Add urls in about

--- src/events.cpp
+++ src/events.cpp
@@ -22,16 +22,8 @@ bool CAJ2PDF::eventFilter(QObject *object, QEvent *event) {
         this, QString::fromUtf8("关于"),
         QString::fromUtf8(
             std::string(
-                "<h2 align=\"center\">关于本项目</h2><br><p "
-                "style=\"line-height:150%\">这是一个免费开源的 CAJ 转 PDF "
-                "转换器，基于 <a "
-                "href=\"https://github.com/caj2pdf/caj2pdf\">cajpdf</a> 和 <a "
-                "href=\"https://mupdf.com/\">mupdf</a> 实现。<br>主页：<a "
-                "href=\"https://caj2pdf-qt.sainnhe.dev\">https://"
-                "caj2pdf-qt.sainnhe.dev</a><br>作者：<a "
-                "href=\"mailto:i@sainnhe.dev\">Sainnhe "
-                "Park</a><br>许可：GPL3<br>版本：" +
-                version + "</p>")
+                "<h2 align=\"center\">关于本项目</h2><br><p style=\"line-height:150%\">这是一个免费开源的 CAJ 转 PDF 转换器，基于 <a href=\"https://github.com/caj2pdf/caj2pdf\">cajpdf</a> 和 <a href=\"https://mupdf.com/\">mupdf</a> 实现。<br>主页：<a href=\"https://caj2pdf-qt.sainnhe.dev\">https://caj2pdf-qt.sainnhe.dev</a><br>作者：<a href=\"mailto:i@sainnhe.dev\">Sainnhe Park</a><br>许可：<a href=\"https://github.com/sainnhe/caj2pdf-qt/blob/master/LICENSE\">GPL3</a><br>版本：<a href=\"https://caj2pdf-qt.sainnhe.dev/CHANGELOG.html\">" +
+                version + "</a></p>")
                 .c_str()));
     return true;
   }
